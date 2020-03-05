2c74ecb44508ebd41a26a56e571cf0bda9967b8a
FrolovBI <bi50_d.s.frolov@mpt.ru>
2020-03-06 02:06:32 +0300

    Frolov Dmitrij made this commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace FilemngFrolov
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
+            this.metroButton2 = new MetroFramework.Controls.MetroButton();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.BackColor = System.Drawing.SystemColors.GrayText;
+            this.treeView1.Location = new System.Drawing.Point(23, 63);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(754, 364);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.DoubleClick += new System.EventHandler(this.treeView1_DoubleClick);
+            // 
+            // metroButton1
+            // 
+            this.metroButton1.Location = new System.Drawing.Point(23, 16);
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.Size = new System.Drawing.Size(216, 41);
+            this.metroButton1.TabIndex = 1;
+            this.metroButton1.Text = "Назад";
+            this.metroButton1.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroButton1.UseSelectable = true;
+            this.metroButton1.Click += new System.EventHandler(this.metroButton1_Click);
+            // 
+            // metroButton2
+            // 
+            this.metroButton2.Location = new System.Drawing.Point(254, 16);
+            this.metroButton2.Name = "metroButton2";
+            this.metroButton2.Size = new System.Drawing.Size(209, 41);
+            this.metroButton2.TabIndex = 2;
+            this.metroButton2.Text = "Копировать путь";
+            this.metroButton2.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroButton2.UseSelectable = true;
+            this.metroButton2.Click += new System.EventHandler(this.metroButton2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.metroButton2);
+            this.Controls.Add(this.metroButton1);
+            this.Controls.Add(this.treeView1);
+            this.ForeColor = System.Drawing.SystemColors.ButtonShadow;
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private MetroFramework.Controls.MetroButton metroButton1;
+        private MetroFramework.Controls.MetroButton metroButton2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace FilemngFrolov
+{
+    public partial class Form1 : MetroFramework.Forms.MetroForm
+    {
+        private string now_path, old_path;
+        public int folders_count = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void go_to_path(string kuda)
+        {
+            treeView1.Nodes.Clear();
+            string[] folders = Directory.GetDirectories(@kuda);
+            string[] files = Directory.GetFiles(@kuda);
+            folders_count = 0;
+            foreach (string folder in folders)
+            {
+                treeView1.Nodes.Add(folder);
+                folders_count++;
+            }
+            foreach (string file in files)
+                treeView1.Nodes.Add(file);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            now_path = "C:\\";
+            go_to_path(@now_path);
+        }
+
+        private void metroComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void metroButton1_Click(object sender, EventArgs e)
+        {
+            string z = now_path;
+            now_path = old_path;
+            go_to_path(@now_path);
+            old_path = z;
+        }
+
+        private void metroButton2_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(@now_path);
+        }
+
+        private void treeView1_DoubleClick(object sender, EventArgs e)
+        {
+            if (treeView1.SelectedNode.Index < folders_count)
+            {
+                old_path = now_path;
+                now_path = @treeView1.SelectedNode.Text.ToString();
+                go_to_path(@now_path);
+            }
+        }
+    }
+}
